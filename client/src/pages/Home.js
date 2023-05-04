56c92e5671786a2c7932eb2cb4e227c4791ee3bd
WinterSunset95 <wintersunset95@gmail.com>
2023-05-04 17:55:52 +0530

    comitting for use

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,6 +5,12 @@ import axios from 'axios';
 const Home = () => {
     const { username, setUsername } = useContext(MainContext)
     const [table, setTable] = useState([])
+    const [input, setInput] = useState(false)
+    const [trade, setTrade] = useState('')
+    const [type, setType] = useState('')
+    const [result, setResult] = useState('')
+    const [timeframe, setTimeframe] = useState('')
+    const [broker, setBroker] = useState('')
     const logout = () => {
         setUsername('')
         localStorage.setItem('username', '')
@@ -18,11 +24,35 @@ const Home = () => {
                 console.log(error)
             })
     }
+    const addToTable = () => {
+        console.log('add to table')
+    }
+    const Button = () => {
+        return (
+            <div className='py-2'>
+                <button onClick={setInput(true)} className='bg-blue-300 px-4 py-2 rounded-lg'>
+                    Update
+                </button>
+            </div>
+        )
+    }
+    const Input = () => {
+        return (
+            <form onSubmit={addToTable()} className='p-2 border border-1 rounded-lg w-fit flex flex-col '>
+                <label htmlFor="trade">Select trade: </label>
+                <select name="options" id="options" value={trade} onChange={(event) => setTrade(event.target.value)}>
+                    <option value="">--- Select ---</option>
+                    <option value="Buy">Buy</option>
+                    <option value="Sell">Sell</option>
+                </select>
+            </form>
+        )
+    }
     const User = () => {
         return(
             <div className='flex flex-col justify-center items-center'>
                 <h1>Welcome</h1>
-                <h2 className='' onClick={logout}>{username}</h2>
+                <h2 className='cursor-pointer' onClick={logout}>{username}</h2>
             </div>
         )
     }
@@ -65,16 +95,17 @@ const Home = () => {
     const Auth = () => {
         return (
             <div>
-                {table.length > 0 ? <Table /> : <div onClick={loadTable}>Click to load table</div>}
+                {table.length > 0 ? <Table /> : <div className="cursor-pointer" onClick={loadTable}>Click to load table</div>}
             </div>
         )
     }
     return(
         <div className='w-full p-4'>
-            <div className='p-2 bg-blue-100 flex flex-row justify-between items-center'>
+            <div className='p-2 bg-blue-100 flex flex-row justify-between items-center mb-2'>
                 <h1>Traders Notes</h1>
                 {username != '' ? <User /> : <Login />}
             </div>
+            {input ? <Button /> : <Input />}
             {username != '' ? <Auth /> : <NonAuth />}
         </div>
     )
